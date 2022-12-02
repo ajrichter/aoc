7516310dc4fb480d7dbc2e73a39ac3e3d709991b
ajrichter <andrew.richter33@gmail.com>
2022-12-02 01:57:50 -0600

    12/1/22 done

--- 1/calories.py
+++ 1/calories.py
@@ -1,12 +1,25 @@
 def getLargestElf():
-    # open file
-    # read in 
-    # local max
-    # calculat
-    open(short.in) as f
+  in_file = "1.in"
+  print("opening file: " + in_file)
+  f = open(in_file, "r")  
+  max = 0
+  currCal = 0
+  all_elves = []
 
-    for(line in f):
-        
+  for l in f:
+    if l == '\n':
+      print("I am carrying " + str(currCal) + " calories!")
+      all_elves.append(currCal)
+      # if currCal > max:
+      #   max = currCal
+      currCal = 0
+    else:
+      print("Added " + l + " calories!")
+      currCal += int(l)
+
+  all_elves.sort(reverse=True)
+  print("top 3 elves are: " + str(all_elves[0:3]))
+  print("sum is: " + str(sum(all_elves[0:3])))
 
 
-getLargestElf
+getLargestElf()
