5f8e7b8904fe5919640d5dd94b2e70ca04e63427
ajrichter <andrew.richter33@gmail.com>
2022-12-03 17:09:43 -0600

    WIP

--- 3.py
+++ 3.py
@@ -1,7 +1,32 @@
-def searchRucksack():
-    
-    pass;
+import requests
 
-# searchRucksack()
-# ord() -96
+def searchRucksack():
+  file = "3.in"
+  f = open(file, "r")
+
+  # NJvhJcQWTJWTNTFFMTqqGqfTmB
+  totalPriority = 0
+  for l in f:
+    length = len(l)/2
+    comp1 = l[:length].sort()
+    comp2 = l[length:].sort()
+    found = false
+    letter = '?'
+    for x in comp1:
+      for y in comp2:
+        if(x is y):
+          letter = '?'
+          print(letter)
+          print(ord(letter)-96)
+          found = true
+          break
+      if(found):
+        break
+    totalPriority += ord(letter)-96
+    
+    # add ord() -96 to the total
+    # add the ord of the letter to total
+  print("total: " + str(totalPriority))
+
+searchRucksack()
 # print(ord('p')-96)
