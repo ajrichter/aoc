417de79f56a10a46536989b5a4275a49f11f06a9
ajrichter <andrew.richter33@gmail.com>
2022-12-03 17:24:31 -0600

    WIP

--- 3.py
+++ 3.py
@@ -7,12 +7,7 @@ def searchRucksack():
   # NJvhJcQWTJWTNTFFMTqqGqfTmB
   totalPriority = 0
   for l in f:
-    l = l.strip()
-    print(l)
-    print(len(l))
     length = int(len(l)/2)
-    print(length)
-    
     comp1 = sorted(l[:length])
     comp2 = sorted(l[length:])
     found = False
@@ -20,7 +15,7 @@ def searchRucksack():
     for x in comp1:
       for y in comp2:
         if(x is y):
-          letter = '?'
+          letter = x
           print(letter)
           print(ord(letter)-96)
           found = True
