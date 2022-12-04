8d1660efe0d2890e958c96ba235b817c174d9bba
ajrichter <andrew.richter33@gmail.com>
2022-12-04 12:33:07 -0600

    WIP

---
+++ 4_1.py
@@ -0,0 +1,21 @@
+def getID():   
+  file = "4.in"
+  f = open(file, "r")
+
+#   2-4,6-8
+  inside = 0
+  for l in f:
+    x = l.split(",")
+    a = x[0].split("-")
+    b = x[1].split("-")
+
+    contains = False
+    if(a[0] <= b[0] and a[1] >= b[1]):
+      inside+=1
+    elif(a[0] >= b[0] and a[1] <= b[1]):
+      inside+=1
+    
+
+  print(inside)
+    
+getID()
