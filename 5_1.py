08dab747d58c476810a3387ccb505715b67d1069
ajrichter <andrew.richter33@gmail.com>
2022-12-05 21:58:33 -0600

    WIP

--- 5_1.py
+++ 5_1.py
@@ -1,8 +1,13 @@
 def starSearch():   
-  file = "5.in"
-  f = open(file, "r")
-  for l in f:
-    print(l.rstrip())
+  infile = "5.in"
+  # f = open(file, "r")
+  data = open(infile).read()
+  lines = data.split('\n]')
+
+  for l in lines:
+    print("clean: " + l.rstrip())
+    # read until char[0] != '['
+
 
 
 starSearch()
