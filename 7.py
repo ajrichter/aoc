888ed1045b7f10a34b856531646535d4f913ed63
ajrichter <andrew.richter33@gmail.com>
2022-12-10 09:40:16 -0600

    wip

--- 7.py
+++ 7.py
@@ -5,8 +5,31 @@ def noSpace():
   
     #   add in files
     # map value to size
-
-
+    # if $ then do x
+    # if # then
+# $ cd /
+# $ ls
+# dir a
+# 14848514 b.txt
+# 8504156 c.dat
+# dir d
+# $ cd a
+# $ ls
+# dir e
+# 29116 f
+# 2557 g
+# 62596 h.lst
+# $ cd e
+# $ ls
+# 584 i
+# $ cd ..
+# $ cd ..
+# $ cd d
+# $ ls
+# 4060174 j
+# 8033020 d.log
+# 5626152 d.ext
+# 7214296 k
 
 
 
--- Readme.md
+++ Readme.md
@@ -11,3 +11,6 @@ data = open(infile).read().strip()
 lines = [x.strip() for x in data.split('\n')]
 ```
 
+# Day 7
+- brute force doesn't seem like a good idea but can code a naive solution to start
+- since the total input is small 
