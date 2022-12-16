c5c3285acf73f387a664d3d1c5cd67a6327dda9d
ajrichter <andrew.richter33@gmail.com>
2022-12-16 15:15:54 -0600

    more logic

--- 7.py
+++ 7.py
@@ -9,11 +9,13 @@ def noSpace():
 
   # $ cd /
   # 012345
-  for l in lines:
+  for i in sizeof(lines):
+    l = lines[i]
     command = l[0]
     if command == '$':
       newCmd = str(l[2:4])
       print("found cmd: " + newCmd)
+
       if newCmd == 'cd':
         newDir = str(l[5:])
         if newDir == '..':
@@ -23,8 +25,13 @@ def noSpace():
           parent_dirs[newDir] = current_dir
           current_dir = newDir
       elif newCmd == 'ls':
-        
-    elif command == 'l':
+        # iterate through dirs until next $
+        temp = i+1
+        l_temp = lines[temp]
+        while()
+        if 
+
+    elif command == 'd':
 
 
 
@@ -34,29 +41,6 @@ def noSpace():
     # map value to size
     # if $ then do x
     # if # then
-# $ cd /
-# $ ls
-# dir a
-# 14848514 b.txt
-# 8504156 c.dat
-# dir d
-# $ cd a
-# $ ls
-# dir e
-# 29116 f
-# 2557 g
-# 62596 h.lst
-# $ cd e
-# $ ls
-# 584 i
-# $ cd ..
-# $ cd ..
-# $ cd d
-# $ ls
-# 4060174 j
-# 8033020 d.log
-# 5626152 d.ext
-# 7214296 k
 
 
 
