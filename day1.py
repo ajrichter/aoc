f9aacc39b4b72b9eba370e93b99e38403f0d5a36
ajrichter <andrew.richter33@gmail.com>
2024-12-01 10:30:45 -0600

    x

--- day1.py
+++ day1.py
@@ -31,7 +31,16 @@ def read_file(file_path):
     for i in range(len(list1)):
         count = 0
         for j in range(len(list2)):
-            if list1[i] == 
+            if list1[i] ==  list2[j]:
+                count += 1
+            if list1[i] < list2[j]:
+                break
+        
+        similar = similar + (count*list1[i])
+
+
+    print(similar)
+    
 
 # Main program
 if __name__ == "__main__":
