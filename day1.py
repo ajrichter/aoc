5596017f7cc66ee69fa33021e504b4071b454f1d
ajrichter <andrew.richter33@gmail.com>
2024-12-01 10:20:54 -0600

    x

--- day1.py
+++ day1.py
@@ -1,15 +1,36 @@
 def read_file(file_path):
+    list1 = []
+    list2 = []
+
     try:
         with open(file_path, 'r') as file:  # Open the file in read mode
             for line in file:  # Read each line in the file
                 line = line.strip()  # Remove leading/trailing whitespace
-                print(f"Line: {line}")  # Process or print the line
+                words = line.split()
+                if words[0].isdigit():
+                    list1.append(int(words[0]))
+                if words[1].isdigit():
+                    list2.append(int(words[1]))
+                else:
+                    print(f"Skipping non-numeric line: {line}")
     except FileNotFoundError:
         print(f"Error: File not found - {file_path}")
     except IOError as e:
         print(f"Error reading file {file_path}: {e}")
 
+    sum = 0
+    list1.sort()
+    list2.sort()
+    print(list1)
+    for i in range(len(list1)):
+        sum = sum + abs(list1[i] - list2[i])
+
+    print(sum)
+
+
+
 # Main program
 if __name__ == "__main__":
-    file_path = "day1.in"  # Replace with your file path
+    file_path = "day1.in"
+    # file_path = "easy1.txt"
     read_file(file_path)
