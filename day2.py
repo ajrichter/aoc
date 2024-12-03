5566d5612659d993253239e3b4c868faade09bdd
ajrichter <andrew.richter33@gmail.com>
2024-12-03 02:05:56 -0600

    x

--- day2.py
+++ day2.py
@@ -1,3 +1,31 @@
+def checkEquals(levels):
+    for x in range(len(levels)-1):
+        a = int(levels[x])
+        b = int(levels[x+1])
+        if(a == b):
+            return False
+    return True
+
+def checkIncrease(levels):
+    if(len(levels) == 1):
+        return True
+    a = int(levels.pop(0))
+    b = int(levels.peek(0))
+    if ((a < b) and (a >= b-3)):
+        return checkIncrease(levels)
+    else:
+        return False
+
+def checkDecrease(levels):
+    if(len(levels) == 1):
+        return True
+    a = int(levels.pop(0))
+    b = int(levels.peek(0))
+    if ((a > b) and (a-3 <= b)):
+        return checkDecrease(levels)
+    else:
+        return False
+
 def run_code(file_path):
     data = open(file_path).read().strip()
     lines = [x.strip() for x in data.split('\n')]
@@ -5,28 +33,9 @@ def run_code(file_path):
 
     for line in lines:
         levels = line.split()
-        isEqual = False
-        isUp = False
-        isDown = False
-
-        for x in range(len(levels)-1):
-            a = int(levels[x])
-            b = int(levels[x+1])
-            if(a == b):
-                isEqual = False
-                break
-            if((b-a) <= 3):
-                isUp = True
-            else:
-                isUp = False
-                break
-            if(a-b) <= 3:
-                isDown = True
-            else:
-                isDown = False
-                break
-
-        if(isEqual and (isUp or isDown)):
+        if not checkEquals(levels):
+            break
+        if(checkIncrease(levels) or checkDecrease(levels)):
             sum+=1
 
     print(sum)
