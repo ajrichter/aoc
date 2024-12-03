7f018452b4fcb33bada9cada001165e921137cfd
ajrichter <andrew.richter33@gmail.com>
2024-12-03 01:28:12 -0600

    x

--- day2.py
+++ day2.py
@@ -1,42 +1,28 @@
+def checkEquals(lines):
+    for x in range(len(lines)-1):
+        if(lines[x] == lines[x+1]):
+            return False
+        
+    return True
+
+def checkIncrease(lines):
+    for x in range(len(lines)-1):
+        if(lines[x] < lines[x]):
+            return False
+    return True
+    
 def read_file(file_path):
-    list1 = []
-    list2 = []
-
-    try:
-        with open(file_path, 'r') as file:  # Open the file in read mode
-            for line in file:  # Read each line in the file
-                line = line.strip()  # Remove leading/trailing whitespace
-                words = line.split()
-                if words[0].isdigit():
-                    list1.append(int(words[0]))
-                if words[1].isdigit():
-                    list2.append(int(words[1]))
-                else:
-                    print(f"Skipping non-numeric line: {line}")
-
+    data = open(file_path).read().strip()
+    lines = [x.strip() for x in data.split('\n')]
     sum = 0
-    list1.sort()
-    list2.sort()
-    for i in range(len(list1)):
-        sum = sum + abs(list1[i] - list2[i])
+
+    for line in lines:
+        levels = line.split()
+        if (checkEquals(levels) and checkIncrease(levels) and checkDecrease(levels)):
+            sum+=1             
 
     print(sum)
 
-    # Part two
-    similar = 0
-    for i in range(len(list1)):
-        count = 0
-        for j in range(len(list2)):
-            if list1[i] ==  list2[j]:
-                count += 1
-            if list1[i] < list2[j]:
-                break
-        
-        similar = similar + (count*list1[i])
-
-
-    print(similar)
-    
 
 # Main program
 if __name__ == "__main__":
