4d659ea93a7326121f6c17e2ede4ec1da35c3fe0
ajrichter <andrew.richter33@gmail.com>
2024-12-03 01:35:06 -0600

    x

--- day2.py
+++ day2.py
@@ -2,7 +2,6 @@ def checkEquals(lines):
     for x in range(len(lines)-1):
         if(lines[x] == lines[x+1]):
             return False
-
     return True
 
 def checkIncrease(lines):
@@ -13,9 +12,9 @@ def checkIncrease(lines):
 
 def checkDecrease(lines):
     for x in range(len(lines)-1):
-        if(lines[x] < lines[x]) and lines[x]+3 <= lines[x]:
-            return False            
-    return True
+        if(lines[x] > lines[x]) and lines[x] >= lines[x]+3:
+            return True            
+    return False
 
 def read_file(file_path):
     data = open(file_path).read().strip()
@@ -28,9 +27,9 @@ def read_file(file_path):
             sum+=1
 
     print(sum)
+    print(2)
 
 # Main program
 if __name__ == "__main__":
     file_path = "day2.in"
-    # file_path = "easy1.txt"
     read_file(file_path)
