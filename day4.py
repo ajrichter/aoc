98358e5aabad673c3e55fc4b0b0184bb30a38f28
ajrichter <andrew.richter33@gmail.com>
2024-12-03 23:35:33 -0600

    x

--- day4.py
+++ day4.py
@@ -1,26 +1,17 @@
-def runPtTwo(file_path):
+def runOne(file_path):
     data = open(file_path).read().strip()
     lines = [x.strip() for x in data.split('\n')]
-    sum = 0
-
+    # convert all lines to a [][] array
+    # do graph search for XMAS
+    # add visited to check nearby?
+    xArray = [][]
     for line in lines:
-        nums = line.split()
-        current = checkLevel(nums)
-        if current == 0:
-            for x in range(len(nums)):
-                new_list = list(nums[:x] + nums[x+1:])
-                current = checkLevel(new_list)
-                if current == 1:
-                    print(new_list)
-                    sum += 1
-                    break
-        else:
-            sum+=1
+        
+        for x in line:
 
-    print(sum)
+
+
 
 if __name__ == "__main__":
-    file_path = "day2.in"
-    # file_path = "easy2.in"
-    # run_code(file_path)
-    runPtTwo(file_path)
+    file_path = "day4.in"
+    runOne(file_path)
