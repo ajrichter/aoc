7f6afdd23000d9d959298e2e02152c9a03c75b12
ajrichter <andrew.richter33@gmail.com>
2024-12-05 23:38:37 -0600

    x

--- day3.py
+++ day3.py
@@ -19,18 +19,16 @@ def mulTwo(file_path):
     
     data = open(file_path).read().strip()
     # split into off patterns
-    off_match = re.findall(off, data)
-    print(off_match)
+    off_match = re.split(off, data)
     # add the sum of index 0 
     matches = re.findall(pattern, off_match[0])
-    print(matches)
     for m in matches:
         result = m[4:-1].split(',')
         sum = sum + (int(result[0])*int(result[1]))
 
     for disabled in off_match[1:]:
         # find the first on
-        on_match = re.findall(on, disabled)
+        on_match = re.split(on, disabled)
         for enabled in on_match[1:]:
             matches = re.findall(pattern, enabled)
             for m in matches:
