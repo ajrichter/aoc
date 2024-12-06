6c7a9dec876899ddce27c0b11ba6c96b65f461b2
ajrichter <andrew.richter33@gmail.com>
2024-12-05 23:20:45 -0600

    x

--- day3.py
+++ day3.py
@@ -1,19 +1,30 @@
 import re
 
 def multiply(file_path):
-    pattern = r"mul\(\d,\d\)"
+    pattern = r"mul\(\d+,\d+\)"
     sum = 0
-
     data = open(file_path).read().strip()    
-    print(data)
-
     matches = re.findall(pattern, data)
     for m in matches:
         result = m[4:-1].split(',')
-        sum = sum + (result[0]*result[1])
-
+        print(result)
+        sum = sum + (int(result[0])*int(result[1]))
     print(sum)
+
+def mulTwo(file_path):
+    pattern = r"mul\(\d+,\d+\)"
+    
+    sum = 0
+    data = open(file_path).read().strip()    
+    matches = re.findall(pattern, data)
+    for m in matches:
+        result = m[4:-1].split(',')
+        print(result)
+        sum = sum + (int(result[0])*int(result[1]))
+
+
 
 if __name__ == "__main__":
     file_path = "day3.in"
     multiply(file_path)
+    mulTwo(file_path)
