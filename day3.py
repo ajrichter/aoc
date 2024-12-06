dda631b42aadddfc036bd8e92db49ffacf64c7be
ajrichter <andrew.richter33@gmail.com>
2024-12-05 23:02:18 -0600

    x

--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
-def runOne(file_path):
+def multiply(file_path):
     data = open(file_path).read().strip()
     lines = [line.strip() for line in data]
 
 
 if __name__ == "__main__":
     file_path = "day3.in"
-    runOne(file_path)
+    multiply(file_path)
