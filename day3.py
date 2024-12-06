f8f001a326bb2c1bb6d803eee0e9a07e04b92647
ajrichter <andrew.richter33@gmail.com>
2024-12-05 22:52:21 -0600

    x

--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
 def runOne(file_path):
     data = open(file_path).read().strip()
-    array_2d = [list(line.strip()) for line in data]
+    lines = [line.strip() for line in data]
 
 
 if __name__ == "__main__":
-    file_path = "day4.in"
+    file_path = "day3.in"
     runOne(file_path)
