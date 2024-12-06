74a0ca229bb7474de06a6df84f2ee90392385b0f
ajrichter <andrew.richter33@gmail.com>
2024-12-05 23:15:10 -0600

    x

--- day3.py
+++ day3.py
@@ -4,15 +4,13 @@ def multiply(file_path):
     pattern = r"mul\(\d,\d\)"
     sum = 0
 
-    data = open(file_path).read().strip()
-    lines = [line.strip() for line in data]    
-    for line in lines:
-        matches = re.findall(pattern, line)
-        for m in matches:
-            print(m)
-            temp = m[4:-1]
-            result = temp.split(',')
-            sum = sum + (result[0]*result[1])
+    data = open(file_path).read().strip()    
+    print(data)
+
+    matches = re.findall(pattern, data)
+    for m in matches:
+        result = m[4:-1].split(',')
+        sum = sum + (result[0]*result[1])
 
     print(sum)
 
